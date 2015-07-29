93970ce2554a4bfc9ee73682f88fdb31539c0849
Bran van der Meer <branmovic@gmail.com>
2015-07-30 00:38:05 +0200

    Refactored to more functional

--- app.js
+++ app.js
@@ -1,93 +1,85 @@
 ///////////////////////////////////////////////////////////////////////////////
 /// [Pure] Time functions
 
-//  toNumber :: String -> Number -> Number
-var toNumber = R.flip(R.curry(parseInt));
-
 //  toInt :: * -> Number
-var toInt = toNumber(10);
+var toInt = R.partial(R.flip(R.curry(parseInt)), 10);
 
 //  zeroPad :: String -> String
 var zeroPad = R.compose(R.slice(-2, Infinity), R.concat('00'));
 
-//  timeToMinutes :: String -> Number
-var timeToMinutes = function(str) {
-    var nums = R.map(toInt, R.split(':', str)); // @todo compose
-    return (nums[0] * 60) + nums[1];
+//  splitTime :: String -> [Number]
+var splitTime = R.compose(R.map(toInt), R.split(':'));
+
+//  parseTime :: String -> Number
+var parseTime = function(str) {
+    var nums = splitTime(str);
+    return R.add(R.multiply(R.head(nums), 60), R.nth(1, nums));
 };
 
-//  minutesToTime :: Number -> String
-var getHours = R.partial(R.flip(R.divide), 60);
-var getMinutes = R.partial(R.flip(R.modulo), 60);
-var minutesToTime = function(num) {
-    var h = R.compose(zeroPad, toInt, getHours);
-    var m = R.compose(zeroPad, getMinutes);
-    return h(num) + ':' + m(num);
-};
-
-//  getTime :: Date -> String
-var getTime = function(t) {
+//  stringifyTime :: Date -> String
+var stringifyTime = function(t) {
     var h = zeroPad(t.getHours());
     var m = zeroPad(t.getMinutes());
     return R.join(':', [h, m]);
 };
 
-// get(540, 500, 600) -> 40%
-var getMinMaxPercentage = function(now, start, end) {
+//  percentage :: Number -> Number -> Number -> Number
+var percentage = function(now, start, end) {
     if (now < start) return 0;
     var result = (now - start) / (end - start) * 100;
     return result > 100 ? 100 : result;
 };
 
-//  getTimeDelta :: Date -> Date -> Number -> Object -> Object
-var getTimeDelta = function(startDate, nowDate, offset, chapter) {
+//  getTimeDelta :: String -> String -> Number -> Object -> Tuple(Number, Object)
+var getTimeDelta = R.curry(function(start, now, offset, chapter) {
 
     var delta = chapter.talk + chapter.exercises + chapter.solutions;
-    var now   = timeToMinutes(getTime(nowDate));
-    var start = offset + timeToMinutes(startDate);
-    var end   = start + delta;
+    var begin = offset + parseTime(start);
+    var end   = begin + delta;
 
-    return {
-        title: chapter.title,
+    var obj = R.merge(chapter, {
         progress: {
-            talk: getMinMaxPercentage(chapter.talk, 0, delta),
-            exercises: getMinMaxPercentage(chapter.exercises, 0, delta),
-            solutions: getMinMaxPercentage(chapter.solutions, 0, delta),
-            current: getMinMaxPercentage(now, start, end)
-        },
-        offset: delta
-    };
+            talk:      percentage(chapter.talk,      0, delta),
+            exercises: percentage(chapter.exercises, 0, delta),
+            solutions: percentage(chapter.solutions, 0, delta),
+            current:   percentage(now, begin, end)
+        }
+    });
+
+    return [offset + delta, obj];
+});
+
+//  getTimeDeltas :: String -> Date -> [Object] -> [Object]
+var getTimeDeltas = function(start, now, chapters) {
+    return R.mapAccum(getTimeDelta(start, parseTime(stringifyTime(now))), 0, chapters)[1];
 };
 
-//  getTimeDeltas :: Date -> Date -> [Object] -> [Object]
-var getTimeDeltas = function(start, now, chapters) {
-    var offset = 0;
-    var newChapters = [];
-    chapters.forEach(function(chapter) {
-        var newChapter = getTimeDelta(start, now, offset, chapter);
-        newChapters.push(newChapter);
-        offset += newChapter.offset;
-    });
-    return newChapters;
+//  getClock :: Date -> String
+var getClock = function(t) {
+    var h = zeroPad(t.getHours());
+    var m = zeroPad(t.getMinutes());
+    var s = zeroPad(t.getSeconds());
+    return R.join(':', [h, m, s]);
 };
 
 
 ///////////////////////////////////////////////////////////////////////////////
 /// [Pure] HTML generation
 
-//  getHTMLDiv :: String -> String -> String
-var getHTMLDiv = function(cn, perc) {
-    return '<div class="{c}" style="width:{p}%"></div>'
+//  getHTMLDiv :: String -> String -> String -> String
+var getHTMLDiv = function(cn, perc, i) {
+    return '<div class="{c}" style="width:{p}%">{i}</div>'
         .replace('{c}', cn)
-        .replace('{p}', perc);
+        .replace('{p}', perc)
+        .replace('{i}', i);
 };
 
 //  getHTMLChapter :: Object -> String
 var getHTMLChapter = function(chapter) {
-    var talk      = getHTMLDiv('talk', chapter.progress.talk);
-    var exercises = getHTMLDiv('exercises', chapter.progress.exercises);
-    var solutions = getHTMLDiv('solutions', chapter.progress.solutions);
-    var bar       = getHTMLDiv('bar', chapter.progress.current);
+    var talk      = getHTMLDiv('talk', chapter.progress.talk, chapter.talk);
+    var exercises = getHTMLDiv('exercises', chapter.progress.exercises, chapter.exercises);
+    var solutions = getHTMLDiv('solutions', chapter.progress.solutions, chapter.solutions);
+    var bar       = getHTMLDiv('bar', chapter.progress.current, '');
     var progress  = '<div class="progress">' + talk + exercises + solutions + bar + '</div>';
     var title     = '<div class="title">' + chapter.title + '</div>';
     var li        = '<li>' + progress + title + '</li>';
@@ -108,10 +100,9 @@ var app = function(start, chapters) {
 
     var run = function() {
         var now = new Date;
-        domClock.innerHTML = getTime(now);
+        domClock.innerHTML = getClock(now);
         domChapters.innerHTML = getHTMLChapters(getTimeDeltas(start, now, chapters));
     };
-    run();
-    //setInterval(run, 50);
+    setInterval(run, 50);
 
 };
